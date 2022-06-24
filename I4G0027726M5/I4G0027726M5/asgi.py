71a06f32543a1672bfe2b5df33e68890d481ec2a
SanaUdoekong <udoekongsana@gmail.com>
2022-06-24 20:38:11 +0100

    chore: completed task

--- I4G0027726M5/I4G0027726M5/asgi.py
+++ I4G0027726M5/I4G0027726M5/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sanaudoekong project.
+ASGI config for I4G0027726M5 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanaudoekong.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G0027726M5.settings')
 
 application = get_asgi_application()
--- I4G0027726M5/I4G0027726M5/wsgi.py
+++ I4G0027726M5/I4G0027726M5/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sanaudoekong project.
+WSGI config for I4G0027726M5 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanaudoekong.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G0027726M5.settings')
 
 application = get_wsgi_application()
